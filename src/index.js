983f400f2185ccc471aabffc8422a0ac32dc045a
Congwen Ma <Congwen.Ma@centro.net>
2017-04-05 12:20:07 -0500

    shepherd

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ ReactDOM.render(
 );
 
 import Tether from 'tether'
+import Shepherd from 'tether-shepherd'
+import 'tether-shepherd/dist/css/shepherd-theme-arrows.css'
+
 // why do you have to scroll
 document.addEventListener('DOMContentLoaded', function(event) {
   new Tether({
@@ -24,4 +27,25 @@ document.addEventListener('DOMContentLoaded', function(event) {
     attachment: 'top right',
     targetAttachment: 'bottom left'
   })
+
+  let tour = new Shepherd.Tour({
+    defaults: {
+      classes: 'shepherd-theme-arrows'
+    }
+  })
+
+  tour.addStep('example', {
+    title: 'Example Shepherd',
+    text: 'Creating a Shepherd is easy too! Just create ...',
+    attachTo: '.hero-example bottom',
+    advanceOn: '.docs-link click'
+  })
+  tour.addStep('example', {
+    title: 'Example Shepherd 2',
+    text: 'Step 2',
+    attachTo: '.hero-example bottom',
+    advanceOn: '.docs-link click'
+  })
+
+  tour.start()
 })
