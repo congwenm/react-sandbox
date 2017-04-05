272e994ba6f97c7651a5518fae1bfd890dbbddb5
Congwen Ma <Congwen.Ma@centro.net>
2017-04-05 11:25:54 -0500

    basic setup of this stuff

--- src/index.js
+++ src/index.js
@@ -7,3 +7,21 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+import Tether from 'tether'
+document.addEventListener('DOMContentLoaded', function(event) {
+  new Tether({
+    element: '.comments',
+    target: '.picture',
+    attachment: 'top right',
+    targetAttachment: 'top left'
+  })
+
+
+  new Tether({
+    element: '.yellowBox',
+    target: '.greenBox',
+    attachment: 'top right',
+    targetAttachment: 'bottom left'
+  })
+})
