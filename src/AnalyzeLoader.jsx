b1ee8fb37db1726a332010fe85ab932e207a1955
Congwen Ma <Congwen.Ma@centro.net>
2017-04-05 10:48:35 -0500

    analyze loader converted from css to scss

---
+++ src/AnalyzeLoader.jsx
@@ -0,0 +1,13 @@
+import './AnalyzeLoader.css'
+import React, { Component } from 'react';
+
+const AnalyzeLoader = () =>
+  <div className="analyze-loader">
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
+
+export default AnalyzeLoader
